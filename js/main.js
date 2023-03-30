59282a71ddcfcb607edef8c25ec14c0bfd62ae3c
Mariacecilia1999 <conticecilia199@gamil.com>
2023-03-30 07:32:50 -0300

    Adding fist exercise using loop forEach

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Practice using loop forEach
+
+//First I access the array that I want to go
+
+const shopping = ['Clothes', 'Food', 'Books']
+shopping.forEach(product => {
+   console.log(product)
+}) 
