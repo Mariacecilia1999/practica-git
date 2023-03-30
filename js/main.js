1f587b8ee467c14e70f93c8c771c7b5af5400005
Mariacecilia1999 <conticecilia199@gamil.com>
2023-03-30 07:05:21 -0300

    Realizando segundo ejercicio utilizando bucle for of

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ const students = ['Maria', "Mariana", "Marcos"]
 for(const student of students){
    console.log(student)
 }
+
+const edades = [20, 30, 40, 21]
+
+for( const edad of edades){
+   console.log(edad)
+}
